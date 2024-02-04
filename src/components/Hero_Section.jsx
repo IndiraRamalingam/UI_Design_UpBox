4e92dc84f1ecd1d59cc081c5d083e348c9962fce
IndiraRamalingam <indiraramalingam93@gmail.com>
2024-02-05 04:29:49 +0530

    changes in text size

--- src/components/Hero_Section.jsx
+++ src/components/Hero_Section.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import box_new from '../assets/box_new.png';
 import box from '../assets/box.png'
 import box_art from '../assets/box-art.png'
 
@@ -7,24 +6,24 @@ function Hero_Section() {
   return (
     <>
       <div className="hero_bg container-fluid">
-       <div className="row">
-        <div className='col-12 col-md-auto col-lg-4'>
-          <div className="about-text">
-                <h1 className="text1 mb-3">Look good without leaving your house.</h1>
-                <p className="text2 mb-3">Upbox is the easiest way to look your best without having to hunt for the perfect makeup combination. Our stylists curate the latest trends and send them directly to your door every month.</p>
-                <a href="#" className="btn btn_pink">SIGN UP <span style={{fontSize:'18px'}}>&gt;</span></a>
+        <div className="row">
+          <div className='col-12 col-md-auto col-lg-4'>
+            <div className="about-text">
+              <h1 className="text1 mb-3">Look good without leaving your house.</h1>
+              <p className="text2 mb-3">Upbox is the easiest way to look your best without having to hunt for the perfect makeup combination. Our stylists curate the latest trends and send them directly to your door every month.</p>
+              <a href="#" className="btn btn_pink">SIGN UP <span style={{ fontSize: '18px' }}>&gt;</span></a>
             </div>
-        </div>
+          </div>
 
-        <div className='col-12 col-lg-8 hero_image'>
-                  <div className="heroImages">
-                    <img src={box} alt="Box_Images" />
-                    <img src={box_art} alt="Box_art_image" />
-                  </div> 
+          <div className='col-12 col-lg-8 hero_image'>
+            <div className="heroImages">
+              <img src={box} alt="Box_Images" />
+              <img src={box_art} alt="Box_art_image" />
+            </div>
+          </div>
+
         </div>
-    
-    </div>
-  </div>
+      </div>
 
     </>
   )
--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,23 +1,24 @@
 import React from 'react'
 import work1 from '../assets/work1.png';
 import work2 from '../assets/work2.png';
-import work2_new from '../assets/work2_new.png';
 import work2_art from '../assets/work2-art.png';
 import work3 from '../assets/work3.png';
 
 
 function Section() {
-  return (
-    <>
-        <div className="container-fluid section_container">
+    return (
+        <>
+            <div className="container-fluid section_container">
 
-            {/* First Row */}
+                {/* First Row */}
                 <div className="row m-5 justify-content-center">
-                    <h1 style={{textAlign:'center', textDecoration: 'underline',
-    textDecorationThickness: '5px'}}>How it Works?</h1>
+                    <h1 style={{
+                        textAlign: 'center', textDecoration: 'underline',
+                        textDecorationThickness: '5px'
+                    }}>How it Works?</h1>
                 </div>
 
-            {/* Second Row */}
+                {/* Second Row */}
                 <div className="row justify-content-center">
                     <div className="col-12 col-lg-5 img_row_1">
                         <div >
@@ -25,38 +26,38 @@ function Section() {
                         </div>
                     </div>
                     <div className="col-12 col-lg-7 mt-5 text_row_1">
-                        <h2>Setup your profile & <br/> preferences</h2>
+                        <h2>Setup your profile & <br /> preferences</h2>
                         <p>Once you create an account, you can start to tell us your likes and dislikes so we can tailor the experience just for you.</p>
                     </div>
                 </div>
 
-            {/* Third Row */}
-               <div className="row justify-content-center">
-                    <div className="col-12 col-lg-1">  
+                {/* Third Row */}
+                <div className="row justify-content-center">
+                    <div className="col-12 col-lg-1">
                     </div>
                     <div className="col-12 col-lg-11">
                         <div className='row'>
-                        <div className='col-12 col-lg-5'></div>
-                        <div className='col-12 col-lg-7 work_container img_row_2'>
-                            <img src={work2_new} className='work_img1'/>
-                            <img src={work2_art} className='work_art_img1'/>
-                        </div> 
-                         </div>   
+                            <div className='col-12 col-lg-5'></div>
+                            <div className='col-12 col-lg-7 work_container img_row_2'>
+                                <img src={work2} className='work_img1' />
+                                <img src={work2_art} className='work_art_img1' />
+                            </div>
+                        </div>
 
                         <div className='row'>
                             <div className='col-12 col-lg-6'></div>
                             <div className='col-12 col-lg-6 text_row_2'>
-                                <h2>Review your<br/> custom box</h2>
+                                <h2>Review your<br /> custom box</h2>
                                 <p>Once we get to know you, we will show you the box we’ve crafted. This is your chance to approve it before we ship it to your house.</p>
                             </div>
                         </div>
                     </div>
                 </div>
 
-            {/* Fourth Row */}
-               <div className="row m-5 justify-content-center">
+                {/* Fourth Row */}
+                <div className="row justify-content-center">
                     <div className="col-12 col-lg-5 img_row_3">
-                        <img className="cropped3" src={work3}/>
+                        <img className="cropped3" src={work3} />
                     </div>
                     <div className="col-12 col-lg-7 mt-5 text_row_3">
                         <h2>Try it on at home</h2>
@@ -64,13 +65,13 @@ function Section() {
                     </div>
                 </div>
 
-            {/* Fifth Row */}
-            <div className='mb-5 mt-5' style={{display:'flex',justifyContent:'center'}}>
-                <button href="#" className="btn btn_pink" style={{textAlign:'center'}}>TRY IT FOR YOURSELF <span style={{fontSize:'18px'}}>&gt;</span></button>
+                {/* Fifth Row */}
+                <div className='mb-5 mt-5' style={{ display: 'flex', justifyContent: 'center' }}>
+                    <button href="#" className="btn btn_pink" style={{ textAlign: 'center' }}>TRY IT FOR YOURSELF <span style={{ fontSize: '18px' }}>&gt;</span></button>
+                </div>
             </div>
-        </div>
-    </>
-  )
+        </>
+    )
 }
 
 export default Section
