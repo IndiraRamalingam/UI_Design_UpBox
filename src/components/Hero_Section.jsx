7f52d93b4adc4e583bd4c480c7a578f2e961fe8a
IndiraRamalingam <indiraramalingam93@gmail.com>
2024-02-05 00:02:26 +0530

    Hero image added

--- src/components/Hero_Section.jsx
+++ src/components/Hero_Section.jsx
@@ -8,7 +8,7 @@ function Hero_Section() {
     <>
       <div className="hero_bg container-fluid">
        <div className="row">
-        <div className='col-12 col- col-lg-4'>
+        <div className='col-12 col-lg-4'>
           <div className="about-text">
                 <h1 className="text1 mb-3">Look good without leaving your house.</h1>
                 <p className="text2 mb-3">Upbox is the easiest way to look your best without having to hunt for the perfect makeup combination. Our stylists curate the latest trends and send them directly to your door every month.</p>
